7b21e2e63ea8f9b5b4ab2564e8a72108bfc6ae1a
YuukiItasha <kuzlikauz@yahoo.de>
2021-12-15 12:24:44 +0100

    Update server.js

--- Aufgabe6/Aufgabe6server/server.js
+++ Aufgabe6/Aufgabe6server/server.js
@@ -12,7 +12,7 @@ var Server;
         let url = new URL(request.url || "", `http://${request.headers.host}`);
         switch (url.pathname) {
             case "/":
-                response.write("Server has been reached");
+                response.write("Server erreichbar");
                 break;
             case "/convertDate":
                 let date = url.searchParams.get("date");
