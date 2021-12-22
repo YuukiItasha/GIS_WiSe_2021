8cd4c02a6e257cfd22e88797f24cfee815dc69bd
YuukiItasha <kuzlikauz@yahoo.de>
2021-12-22 11:53:55 +0100

    Update server.ts

--- Aufgabe6/Aufgabe6server/server.ts
+++ Aufgabe6/Aufgabe6server/server.ts
@@ -1,40 +1,38 @@
 import * as http from "http";
 
-
 namespace Server {
-    const hostname: string = "127.0.0.1";
-    const port: number = 3000;
+  
+  const hostname: string = "127.0.0.1";
+  const port: number = 3000;
 
-    const server: http.Server = http.createServer(
-        (request: http.IncomingMessage, response: http.ServerResponse) => {
+  const server: http.Server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {
 
-        response.statusCode = 200;
-        response.setHeader("Content-Type", "text/plain");
-        response.setHeader("Access-Control-Allow-Origin", "*");
+      response.statusCode = 200; 
 
+      response.setHeader("Content-Type", "text/plain"); 
+      response.setHeader("Access-Control-Allow-Origin", "*"); 
+      let url: URL = new URL(request.url || "",`http://${request.headers.host}`);
 
-        let url: URL = new URL(request.url || "", `http://${request.headers.host}`);
-
-        switch (url.pathname) {
-            case "/":
-                response.write("Server erreichbar");
-                break;
-            case "/convertDate":
-                let date: string = url.searchParams.get("date");
-                let month: string = url.searchParams.get("month");
-                let year: string = url.searchParams.get("year");
-                response.write("Day:" + date + "; Month" + month + "; Year" + year);
-                break;
-
-            default:
-                response.statusCode = 404;
-        }
-        response.end();
+      switch (url.pathname) {
+        case "/": 
+          response.write("Server erreichbar");
+          break;
+        case "/convertDate": 
+          let date: string = url.searchParams.get("date"); 
+          console.log("Das eingegebene Datum lautet: " + date); 
+          response.write("Das eingegebene Datum lautet: " + date); 
+          break;
+        default:
+          response.statusCode = 404; 
+      }
+      response.end(); 
     }
+  );
 
-    );
-    server.listen(port, hostname, () => {
-        console.log(`Server at http://${hostname}:${port}`);
-    });
-
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`); //Wenn der Server erreichbar ist, soll folgendes ausgegeben werden.
+  });
 }
+
