59f4da8d641d40fdc8aa6698f70ff8255d8e215d
YuukiItasha <kuzlikauz@yahoo.de>
2022-01-05 16:42:19 +0100

    Update server.js

--- Aufgabe7/server.js
+++ Aufgabe7/server.js
@@ -1,2 +1,51 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+const mongo = require("mongodb");
+const hostname = "127.0.0.1";
+const port = 3000;
+const mongoUrl = "mongodb://localhost:27017";
+let mongoClient = new mongo.MongoClient(mongoUrl);
+async function dbFind(db, collection, requestObject, response) {
+    await mongoClient.connect();
+    let result = await mongoClient
+        .db(db)
+        .collection(collection)
+        .find(requestObject)
+        .toArray();
+    response.setHeader("Content-Type", "application/json");
+    response.write(JSON.stringify(result));
+}
+async function dbAddOrEdit(db, collection, request) {
+    let jsonString = "";
+    request.on("data", data => {
+        jsonString += data;
+    });
+}
+const server = http.createServer(async (request, response) => {
+    response.statusCode = 200;
+    let url = new URL(request.url || "", `http://${request.headers.host}`);
+    switch (url.pathname) {
+        case "/concertEvents": {
+            await mongoClient.connect();
+            switch (request.method) {
+                case "GET":
+                    await dbFind("interpret", "price", {
+                        price: Number(url.searchParams.get("price"))
+                    }, response);
+                    break;
+                case "POST":
+                    await dbAddOrEdit("interpret", "price", request);
+                    break;
+            }
+            break;
+        }
+        default:
+            response.statusCode = 404;
+    }
+    response.end();
+});
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
 //# sourceMappingURL=server.js.map
