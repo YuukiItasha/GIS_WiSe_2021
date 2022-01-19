9d4a76f32201de6093b7c9efe5fca09455b4cab1
YuukiItasha <kuzlikauz@yahoo.de>
2022-01-19 19:21:09 +0100

    update

--- Aufgabe7/client.ts
+++ Aufgabe7/client.ts
@@ -56,7 +56,7 @@ function mybuttonHandler(): void {
 
 async function post(konzertEvent: KonzertEvent): Promise<void> {
     console.log(konzertEvent);
-    await fetch("http://localhost:5500/concertEvents", {
+    await fetch("http://localhost:8100/EventOr", {
         method: "POST",
         body: JSON.stringify(konzertEvent)
     });
@@ -66,7 +66,7 @@ async function post(konzertEvent: KonzertEvent): Promise<void> {
 
 async function get(): Promise<KonzertEvent[]> {
 
-    let response: Response = await fetch("http://localhost:5500/concertEvents", {
+    let response: Response = await fetch("http://localhost:8100/EventOr", {
         method: "GET"
     });
 
